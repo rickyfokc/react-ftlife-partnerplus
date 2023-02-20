c75fe8f177ea00b69ea7f570685fe18c033537ec
KelvinLamHK <kelvinlamtatyin@gmail.com>
2023-02-20 10:14:44 +0800

    commit

--- src/page/LandingPage.js
+++ src/page/LandingPage.js
@@ -3,6 +3,8 @@ import Cookies from "js-cookie";
 import PlusNavbar from "../components/PlusNavbar";
 import LoadingScreen from "../components/LoadingScreen";
 import "bootstrap/dist/css/bootstrap.css";
+import "../css/LandingPagecss.css"
+
 
 function LandingPage() {
   const [username, setUsername] = useState("");
@@ -38,31 +40,60 @@ function LandingPage() {
       ) : (
         <>
           <PlusNavbar username={username} />
-          <div class=" border-gray-900 border-2 p-3 md:space-x-2 md:flex md:flex-row ">
-            <div class=" md:p-4 md:border-green-500 space-y-2 md:border-2 md:h-auto md:w-3/4 md:flex md:flex-col ">
-              <div class=" md:p-4 md:border-pink-500 md:border-2 md:h-screen md:flex md:flex-col space-y-2">
-                <div class="p-3 border-purple-500 border-2 h-96 md:h-3/5">
-                  Broker Communication
+          <div class="p-3 md:space-x-2 md:flex md:flex-row ">
+            <div className="space-y-2 md:h-auto md:w-3/4 md:flex md:flex-col ">
+              <div className="md:h-screen md:flex md:flex-col space-y-2">
+                <div className="h-96 md:h-3/5 relative flex justify-center">
+                  <div className="test h-16 absolute ">
+                    <span class="bold h4 text-white">Brokers Communications</span>
+                  </div>
+                  <div className=" rounded shadow-lg w-full mt-3 ">
+                  </div>
                 </div>
-                <div class="p-3 border-purple-500 border-2 h-96 md:h-2/5">
-                  Latest Promo
+                <div class="h-96 md:h-2/5 relative flex justify-center">
+                <div className="test h-16 absolute ">
+                    <span class="bold h4 text-white">Latest Promo</span>
+                  </div>
+                  <div className=" rounded shadow-lg w-full mt-3 ">
+                  </div>
+
                 </div>
               </div>
-              <div class=" md:p-4 md:border-yellow-300 md:border-2 md:h-screen md:flex md:flex-row md:space-x-2 ">
-                <div class="p-3 border-purple-500 border-2 md:w-1/2 md:h-full h-96">
-                  Event Calendar
+              <div class="md:h-screen md:flex md:flex-row md:space-x-2 ">
+                <div class="md:w-1/2 md:h-full h-96 relative flex justify-center">
+                <div className="test h-16 absolute ">
+                    <span class="bold h4 text-white">Event Calendar</span>
+                  </div>
+                  <div className=" rounded shadow-lg w-full mt-3 ">
+                  </div>
                 </div>
-                <div class="p-3 border-purple-500 border-2 md:w-1/2 md:h-full h-96">
-                  Qucik Links
+                <div class="md:w-1/2 md:h-full h-96 relative flex justify-center">
+                  <div className="test h-16 absolute ">
+                    <span class="bold h4 text-white">Qucik Links</span>
+                  </div>
+                  <div className=" rounded shadow-lg w-full mt-3 ">
+                  </div>
                 </div>
               </div>
             </div>
-            <div class=" md:p-4 md:border-blue-500 md:border-2 md:h-full md:w-1/4 md:flex md:flex-col md:items-center space-y-2">
-              <div class="border-purple-500 border-2 w-full h-36 md:h-72 p-4">
-                Policy Inquiry
-              </div>
-              <div class="border-purple-500 border-2 w-full h-96 md:h-screen p-4">
-                Document Centre
+            <div class="md:h-full md:w-1/4 md:flex md:flex-col md:items-center space-y-2">
+              <div class="w-full h-36 md:h-72 relative flex justify-center">
+                  <div className="test h-16 absolute ">
+                    <span class="bold h4 text-white">Policy Inquiry</span>
+                  </div>
+                  <div className=" rounded shadow-lg w-full mt-3 ">
+                      
+                  
+                  </div>
+                </div>
+              <div class="w-full h-96 md:h-screen relative flex justify-center">
+                <div className="test h-16 absolute ">
+                    <span class="bold h4 text-white">Document Centre</span>
+                  </div>
+                  <div className=" rounded shadow-lg w-full mt-3 ">
+                      
+                  
+                  </div>
               </div>
             </div>
           </div>
